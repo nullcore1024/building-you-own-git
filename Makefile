72d61f5f8c0fdd59f5037acf75a2b2abd29cabfc
shaobingxie <shaobingxie@gmail.com>
2021-03-01 20:10:41 +0800

    desc: add init cmd

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	go build .
+clean:
+	rm -rf building-you-own-git
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nullcore1024/building-you-own-git/cmd"
+
+func main() {
+	cmd.Execute()
+}
